eb005a0a69dfb2b934136965e9c40f56765cc9f5
Megen Ward <103354733+mward60@users.noreply.github.com>
2024-09-20 20:52:00 -0500

    Update Form1.cs
    Added a comment

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ namespace PayrollTeam3Project
 {
     public partial class Form1 : Form
     {
+    // this is how you add your code I think? you make a branch the it will need to be accepted I think..idk 
+    
         public Form1()
         {
             InitializeComponent();  
